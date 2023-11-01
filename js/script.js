f7fc6642ac879b024301c8608b36bb7fdc42be0c
venupriya1 <priyanikavenu@gmail.com>
2023-10-31 19:08:36 -0700

    new commit

--- js/script.js
+++ js/script.js
@@ -2,66 +2,95 @@ class player{
     constructor(name){
     this.name=name;
     this.score=[];
+    this.finalscore = 0;
     };
 
-    rollDice(){
-        
-        const score = Math.floor(Math.random() * 6) + 1
-        console.log(score);
-        return score;
-    };
 
     buildScore(score){
         this.score.push(score);
         return this.score;
     }
 
+    sumScores(score){
+        this.score = score;
+        for (i=0;i<this.score.length;i++){
+            this.finalscore += this.score[i];
+        }
+        console.log(this.finalscore);
+        return this.finalscore;
+    }
 
 }
-// class dice{
-//     constructor(number){
-//         this.number=number
-//     }
-// }
+class Dice{
+    constructor(){
+      this.score = 0
+    }
+
+    rollDice(){
+        
+        this.score = Math.floor(Math.random() * 6) + 1
+        //console.log(score);
+        return this.score;
+    };
+    }
+//-----------------------------------------
+
 let player1=new player('Ann')
 console.log(player1);
 
 let player2=new player('Sam')
 console.log(player2);
 
+let dice = new Dice();
+
 let goAhead = 'yes';
 let player1Played = false;
 let player2Played = false;
 let player1ScoreAll = [];
 let player2ScoreAll = [];
+var finalscore1 = 0;
+var finalscore2 = 0;
 
 for (i=0;i<5;i++){
-        console.log(i)
+        console.log("round no. :" + i);
 
-        if (goAhead == 'yes' && player1Played == false){
-            const player1Score = player1.rollDice();
+        if (player1Played == false){
+            const player1Score = dice.rollDice();
             console.log("Player1 Score: " + player1Score);
             player1Played = true; 
             player1ScoreAll = player1.buildScore(player1Score);
-            console.log(player1ScoreAll);
+            //console.log(player1ScoreAll);
             
         } 
 
-        if (goAhead == 'yes' && player2Played == false){
-            const player2Score = player2.rollDice();
+        if (player2Played == false){
+            const player2Score = dice.rollDice();
             console.log("Player2 Score: " + player2Score);
             player2Played = true;
             player2ScoreAll = player2.buildScore(player2Score);
-            console.log(player2ScoreAll);
+            //console.log(player2ScoreAll);
         }
 
-        if (goAhead = 'yes' && player1Played && player2Played){
+        if (player1Played && player2Played){
             //goAhead ='no';
             player1Played = false;
             player2Played = false;
-            console.log('final loop')
-            console.log(player1Played)
-            console.log(player2Played)
         }
-};
 
+};
+console.log(player1ScoreAll);
+console.log(player2ScoreAll);
+
+finalscore1 = player1.sumScores(player1ScoreAll);
+finalscore2 = player2.sumScores(player2ScoreAll);
+console.log("final score 1: " + finalscore1);
+console.log("final score 2: " + finalscore2);
+if (finalscore1 > finalscore2){
+    console.log(player1.name + " is winner with highest score: " + player1.finalscore);
+}
+else if(finalscore1 < finalscore2){
+    console.log(player2.name+ " is winner with highest score " + player2.finalscore);
+}
+else if(finalscore1 == finalscore2){
+    console.log("its a tie with equal scores");
+}
