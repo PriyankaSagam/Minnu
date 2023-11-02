b3e32a4eb4c35c83ea4e8aa1370abc08d3013e10
venupriya1 <priyanikavenu@gmail.com>
2023-11-02 07:43:38 -0700

    dom changes

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ class player{
 
     sumScores(score){
         this.score = score;
-        for (i=0;i<this.score.length;i++){
+        for (let i=0;i<this.score.length;i++){
             this.finalscore += this.score[i];
         }
         console.log(this.finalscore);
@@ -34,63 +34,126 @@ class Dice{
     };
     }
 //-----------------------------------------
-
-let player1=new player('Ann')
+let player1=new player('Player1')
 console.log(player1);
 
-let player2=new player('Sam')
+let player2=new player('Player2')
 console.log(player2);
 
 let dice = new Dice();
 
-let goAhead = 'yes';
-let AnnPlayed = false;
-let SamPlayed = false;
-let AnnScoreAll = [];
-let SamScoreAll = [];
-var finalscoreAnn = 0;
-var finalscoreSam = 0;
 
-for (i=1;i<6;i++){
-        console.log("round no. :" + i);
+let player1Played = false;
+let player2Played = false;
+let player1ScoreAll = [];
+let player2ScoreAll = [];
+var player1FinalScore = 0;
+var player2FinalScore = 0;
+let winnerName = '';
 
-        if (AnnPlayed == false){
-            const AnnScore = dice.rollDice();
-            console.log("Ann Score: " + AnnScore);
-            AnnPlayed = true; 
-            AnnScoreAll = player1.buildScore(AnnScore);
-            //console.log(AnnScoreAll);
-            
-        } 
+let numOfRounds = 0;
+const enter = document.getElementById('display');
 
-        if (SamPlayed == false){
-            const SamScore = dice.rollDice();
-            console.log("Sam Score: " + SamScore);
-            SamPlayed = true;
-            SamScoreAll = player2.buildScore(SamScore);
-            //console.log(SamScoreAll);
-        }
+enter.addEventListener('click', function(){
+    document.getElementById('result').textContent = "Click on Start Button to start Game";
+    numOfRounds = document.querySelector('input').value;
+    console.log(numOfRounds);
+})
 
-        if (AnnPlayed && SamPlayed){
-            //goAhead ='no';
-            AnnPlayed = false;
-            SamPlayed = false;
-        }
-
-};
-console.log(AnnScoreAll);
-console.log(SamScoreAll);
-
-finalscoreAnn = player1.sumScores(AnnScoreAll);
-finalscoreSam = player2.sumScores(SamScoreAll);
-console.log("final score of Ann: " + finalscoreAnn);
-console.log("final score of Sam: " + finalscoreSam);
-if (finalscoreAnn > finalscoreSam){
-    console.log(player1.name + " is winner with highest score: " + player1.finalscore);
+if (isFinite(numOfRounds) && !isNaN(numOfRounds)){
+    console.log(numOfRounds + " is valid number")
+    if (numOfRounds > 100){
+        console.log("please enter value less than or equal 100")
+    }
 }
-else if(finalscoreAnn < finalscoreSam){
-    console.log(player2.name+ " is winner with highest score " + player2.finalscore);
+else{ 
+    console.log("Please input a valid number")
+
 }
-else if(finalscoreAnn == finalscoreSam){
-    console.log("its a tie with equal scores");
-}
+
+const start = document.getElementById('start');
+
+start.addEventListener('click', function(){
+    
+    for (i=1;i<= numOfRounds;i++){
+            console.log("round no. :" + i);
+            document.getElementById('start').disabled = true;
+            if (player1Played == false){
+                const player1Score = dice.rollDice();
+                console.log("Player1 Score: " + player1Score);
+                player1Played = true; 
+                player1ScoreAll = player1.buildScore(player1Score);
+                //console.log(player1ScoreAll);
+                
+            } 
+    
+            if (player2Played == false){
+                const player2Score = dice.rollDice();
+                console.log("Player2 Score: " + player2Score);
+                player2Played = true;
+                player2ScoreAll = player2.buildScore(player2Score);
+                //console.log(player2ScoreAll);
+            }
+    
+            if (player1Played && player2Played){
+                //goAhead ='no';
+                player1Played = false;
+                player2Played = false;
+            }
+    
+    };
+    document.getElementById('result').textContent = "Click on winner button to know the winner";
+})
+
+const winner = document.getElementById('winner');
+
+winner.addEventListener('click', function(){
+    player1FinalScore = 0;
+    player2FinalScore = 0;
+    player1FinalScore = player1.sumScores(player1ScoreAll);
+    document.getElementById('score1').textContent = player1FinalScore;
+    
+    player2FinalScore = player2.sumScores(player2ScoreAll);
+    document.getElementById('score2').textContent = player2FinalScore;
+
+    console.log("final score of Player1: " + player1FinalScore);
+    console.log("final score of Player2: " + player2FinalScore);
+    if (player1FinalScore > player2FinalScore){
+        console.log(player1.name + " is winner with highest score: " + player1.finalscore);
+        winnerName = 'Player1 is the Winner';
+
+    }
+    else if(player1FinalScore < player2FinalScore){
+        console.log(player2.name+ " is winner with highest score " + player2.finalscore);
+        winnerName = 'Player2 is the Winner';
+    }
+    else if(player1FinalScore == player2FinalScore){
+        console.log("its a tie with equal scores");
+        winnerName = 'It is a tie!';
+    }
+
+    document.getElementById('result').textContent = winnerName;
+    document.getElementById('finaltext').textContent = "Hit continue button to continue or Quit";
+
+});
+
+const contue = document.getElementById('continue');
+
+contue.addEventListener('click', function(){
+    document.getElementById('result').textContent = "Choose Number of Rounds for next game and hit Enter";
+    document.getElementById('finaltext').textContent = "";
+    document.getElementById('start').disabled = false;
+    document.querySelector('input').value = ""
+   
+})
+const qut = document.getElementById('quit');
+
+qut.addEventListener('click', function(){
+    document.getElementById('display').disabled = true;
+    document.getElementById('start').disabled = true;
+    document.getElementById('continue').disabled = true;
+    document.getElementById('winner').disabled = true;
+    document.getElementById('result').textContent = "All the buttons disabled as player wants to Quit";
+    document.getElementById('finaltext').textContent = "Refresh page to start playing again";
+ 
+})
